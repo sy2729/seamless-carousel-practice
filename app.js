ae0ba92a3787046159f11cb4d30525a8077a971a
sy2729 <sy2729@tc.columbia.edu>
2018-05-20 21:07:17 -0400

    resolve conflicts

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ for(let i = 0; i < length; i++) {
 }
 
 init();
-<<<<<<< HEAD
+
 let n = 1;
 setInterval(()=> {
     after($(`.carousel > img:nth-child(${setIndex(n)})`))
@@ -14,21 +14,12 @@ setInterval(()=> {
     activate(n%3)
     n += 1;
 }, 2000)
-=======
-
-let n = 0;
-setInterval(()=> {
-    after($(`.carousel > img:nth(${getIndex(n)})`));
-    current($(`.carousel > img:nth(${getIndex(n+1)})`));
-    n += 1;
-    changeDot(n);
-}, 4000)
->>>>>>> backup
 
 
 
 
-<<<<<<< HEAD
+
+
 function current(node) {
     node.removeClass('before').addClass('current');
 }
@@ -60,37 +51,3 @@ function setIndex(n) {
 function activate(n) {
     $('.buttonWrap > div').eq(n).addClass('active').siblings().removeClass('active');
 }
-=======
-
-//API
-function current(node) {
-    node.addClass('current').removeClass('before after')
-        
-}
-function before(node) {
-    node.removeClass('after').addClass('before')
-}
-function after(node) {
-    node.addClass('after').removeClass('current')
-        .one('transitionend', (e)=> {
-                before($(e.currentTarget))
-            })
-}
-function changeDot(index) {
-    $('.buttonWrap > div').eq(getIndex(index)).addClass('active').siblings().removeClass('active')
-}
-
-
-function init() {
-    $('.carousel > img:nth(0)').addClass('current');
-    $('.carousel > img:nth(1)').addClass('before');
-    $('.buttonWrap > div').eq(0).addClass('active')
-}
-
-function getIndex(n) {
-    if(n >= 3) {
-        n = n % 3;
-    }
-    return n;
-}
->>>>>>> backup
