32c08f711f2c86c5bae2923bccb2e30c0fb036ff
sy2729 <sy2729@tc.columbia.edu>
2018-05-20 21:01:33 -0400

    init

--- app.js
+++ app.js
@@ -5,83 +5,46 @@ for(let i = 0; i < length; i++) {
     $('.buttonWrap').append(button);
 }
 
-// hardcode the trigger;
-// $('.btn1').on('click', function() {
-//     $('.carousel').css(
-//         { transform: `translateX(${-530 * 0}px)`}
-//     )
-// })
-// $('.btn2').on('click', function() {
-//     $('.carousel').css(
-//         {transform: `translateX(${-530 * 1}px)`}
-//     )
-// })
-// $('.btn3').on('click', function() {
-//     $('.carousel').css(
-//         { transform: `translateX(${-530 * 2}px)`}
-//     )
-// })
-// $('.btn4').on('click', function() {
-//     $('.carousel').css(
-//         { transform: `translateX(${-530 * 3}px)`}
-//     )
-// })
+init();
+let n = 1;
+setInterval(()=> {
+    after($(`.carousel > img:nth-child(${setIndex(n)})`))
+    current($(`.carousel > img:nth-child(${setIndex(n+1)})`))
+    activate(n%3)
+    n += 1;
+}, 2000)
 
 
 
 
-var allButtons = $('.buttonWrap > div');
-setClass($('.buttonWrap > div'), 0, 'active');
-for (let i = 0; i < allButtons.length; i++) {
-    $(allButtons[i]).on('click', function(e) {
-        //DOM Method
-        // let n;
-        // var target = e.currentTarget;
-        // var siblings = target.parentNode.children;
-        // for (let i = 0; i < siblings.length; i++) {
-        //     if(siblings[i] === target) {
-        //         n = i;
-        //         break;
-        //     }
-        // }
-        // console.log(n)
-
-        //JQuery
-        let n = $(e.currentTarget).index();
-        $('.carousel').css({
-            transform: `translateX(${-n*530}px)`
-        })
-        setClass($('.buttonWrap > div'), n, 'active');
-    })
+function current(node) {
+    node.removeClass('before').addClass('current');
+}
+function before(node) {
+    node.removeClass('after').addClass('before');
+}
+function after(node) {
+    node.removeClass('current').addClass('after').one('transitionend', (e)=> {
+        before($(e.currentTarget));
+    });
 }
 
-var id = 0;
-var timer = setInterval(() => {
-    id++;
-    triggerButton($('.buttonWrap > div'), id % 4, 'click');
-    setClass($('.buttonWrap > div'), id % 4, 'active');
-}, 4000)
-
-$('.carousel').on('mouseenter', function() {
-    window.clearInterval(timer)
-});
-
-$('.carousel').on('mouseout', function () {
-    timer = setInterval(() => {
-        id++;
-        triggerButton($('.buttonWrap > div'), id % 4, 'click');
-        setClass($('.buttonWrap > div'), id%4, 'active');
-    }, 4000)    
-})
-
-
-// reusable function
-function triggerButton (node, index, event) {
-    node.eq(index).trigger(event);
+function init() {
+    $('.carousel > img').eq(0).addClass('current');
+    $('.carousel > img').eq(1).addClass('before');
+    activate(0);
 }
 
+function setIndex(n) {
+    if (n > 3) {
+        n = n % 3
+        if (n ===0 ) {
+            n = 3;
+        }
+    }
+    return n;
+}
 
-function setClass(node, index, className) {
-    node.eq(index).addClass('active')
-        .siblings().removeClass(className);
+function activate(n) {
+    $('.buttonWrap > div').eq(n).addClass('active').siblings().removeClass('active');
 }
